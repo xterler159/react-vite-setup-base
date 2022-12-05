2b9a35d7d15e3d456d2d7c57819a05ea7893c141
Kevin FERNANDO <kevin.fernando@supinfo.com>
2022-12-05 14:51:02 +0100

    removed useless test case

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -9,11 +9,4 @@ describe('Test <App />', () => {
     const appTitle = screen.getByTestId("Hello-World-Title")
     expect(appTitle).toBeInTheDocument()
   })
-
-  it('should show error message within vitest ui', () => {
-    // const { debug } = render(<App />)
-    render(<App />)
-    const appTitle = screen.getByTestId("Hello-World-Titl")
-    expect(appTitle).toBeInTheDocument()
-  })
 })
